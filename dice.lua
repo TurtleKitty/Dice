67a48341a8406f382ecbb7ede4f38d2226c2d494
TurtleKitty <joshua@anwu.org>
2012-05-09 01:39:33 -0400

    Lua version online.

--- dice.lua
+++ dice.lua
@@ -1,17 +1,41 @@
 #!/usr/bin/env lua
 
 
+function inspect (t)
+    for i, xi in pairs(t) do
+	print(i, xi) 
+    end
+end
+
+
 function main (argv)
-    rolls, constant = parse(argv)
+    local rolls, constant = parse(argv)
 
-    polys = mkpolys(rolls)
+    local min = 0
+    local max = 0
+    local combinations = 1;
+    for d,n in pairs(rolls) do
+	min = min + n
+	max = max + n * d
+	combinations = combinations * d^n
+    end
 
-    combinations = 1;
+    local polys = mkpolys(rolls)
 
-    biggun = { 1 }
+    local biggun = { 1 }
  
     for i, x in pairs(polys) do
 	biggun = polymul(biggun, x)
+    end
+
+    for x = min, max+1 do
+	local coeff = biggun[x]
+
+	if coeff then
+	    local px    = coeff / combinations
+	    local hg    = histogram(px)
+	    print( string.format("%d\t\t%.5f\t\t%s", x-1+constant, px, hg) )
+	end
     end
 end
 
@@ -57,8 +81,8 @@ end
 function polymul (x, y)
     local rv = {};
 
-    for i, xi in ipairs(x) do
-	for j, yj in ipairs(y) do
+    for i, xi in pairs(x) do
+	for j, yj in pairs(y) do
 	    local cell = i + j
 	    if rv[cell] == nil then
 		rv[cell] = 0;
@@ -72,4 +96,18 @@ function polymul (x, y)
 end
 
 
+function histogram (n)
+    local num = (0.5 + (500 * n));
+
+    local tmp = {}
+    for i = 0, num do
+	tmp[i] = "#"
+    end
+
+    return table.concat(tmp, "")
+end
+
+
 main(arg)
+
+
